f3f4a4cd332d8a749f9f8e59a26ee5e9f110851a
slowdream <slowdream@yandex.ru>
2017-08-23 15:47:48 +0300

    Исправление ошибок

--- app/Helpers/CashCode.php
+++ app/Helpers/CashCode.php
@@ -1,35 +1,60 @@
 <?php
 
 namespace App\Helpers;
-require_once base_path().'/app/libs/cashcode/cashcode.php';
-require_once base_path().'/app/libs/cashcode/cashcodes.php';
-//use App\libs\cashcode\CashValidator;
+//require_once base_path().'/app/libs/cashcode/CashValidator.php';
+//require_once base_path().'/app/libs/cashcode/cashcodes.php';
+use App\libs\cashcode\CashValidator;
+use App\libs\cashcode\cashcodes;
 
 class CashCode{
 
 	use cashcodes;
-	public $info = ['error' => ''];
-	private $validator = new CashValidator();
+	public $info = [];
+	private $BillToBill_CMD = [];
+	private $validator;
 	private $cash;
-
 
 	public function __construct($cashClass)
 	{
 		$this->cash = $cashClass;
+		$this->validator = new CashValidator();
+
+		// $this->BillToBill_CMD = [
+	 //        "ACK" => pack("c*", 0x02, 0x03, 0x06, 0x00, 0xC2, 0x82),
+	 //        "Reset" => pack("c*", 0x02, 0x03, 0x06, 0x30, 0x41, 0xB3),
+	 //        "GetStatus" => pack("c*", 0x02, 0x03, 0x06, 0x31, 0xC8, 0xA2),
+	 //        "SetSecurity" => pack("c*", 0x02, 0x03, 0x06, 0x32, 0x53, 0x90),
+	 //        "Poll" => pack("c*", 0x02, 0x03, 0x06, 0x33, 0xDA, 0x81),
+	 //        "EnableBillTypes" => pack("c*", 0x02, 0x03, 0x0C, 0x34, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0xB5, 0xC1),
+	 //        "EnableBillTypesEscrow" => pack("c*", 0x02, 0x03, 0x0C, 0x34, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFE, 0xF7),
+	 //        "DisableBillTypes" => pack("c*", 0x02, 0x03, 0x0C, 0x34, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x17, 0x0C),
+	 //        "Stack" => pack("c*", 0x02, 0x03, 0x06, 0x35, 0xEC, 0xE4),
+	 //        "Return" => pack("c*", 0x02, 0x03, 0x06, 0x36, 0x77, 0xD6),
+	 //        "Identification" => pack("c*", 0x02, 0x03, 0x06, 0x37, 0xFE, 0xC7),
+	 //        "Hold" => pack("c*", 0x02, 0x03, 0x06, 0x38, 0x09, 0x3F),
+	 //        "CassetteStatus" => pack("c*", 0x02, 0x03, 0x06, 0x3B, 0x92, 0x0D),
+	 //        "Dispense" => pack("c*", 0x02, 0x03, 0x06, 0x3C, 0x2D, 0x79),
+	 //        "Unload" => pack("c*", 0x02, 0x03, 0x06, 0x3D, 0xA4, 0x68),
+	 //        "EscrowCassetteStatus" => pack("c*", 0x02, 0x03, 0x06, 0x3E, 0x3F, 0x5A),
+	 //        "EscrowCassetteUnload" => pack("c*", 0x02, 0x03, 0x06, 0x3F, 0xB6, 0x4B),
+	 //        "SetCassetteType" => pack("c*", 0x02, 0x03, 0x06, 0x40, 0xC6, 0xC0),
+	 //        "GetBillTable" => pack("c*", 0x02, 0x03, 0x06, 0x41, 0x4F, 0xD1),
+	 //        "Download" => pack("c*", 0x02, 0x03, 0x06, 0x50, 0x47, 0xD0),
+	 //    ];
 	}
 		
 	public function __destruct()
 	{
-		$this->open();
+		$this->validator->open();
 		$this->validator->ExecuteCommand($this->BillToBill_CMD["Reset"]);
 	}
 	public function info($val)
 	{
-		$this->info = array_merge($this->info, $val)
+		$this->info = array_merge($this->info, $val);
 	}
 	public function start() {
 		$this->info(['info' => "Try open..."]);
-		if (!$this->open()){
+		if (!$this->s->open()){
 			$this->info['error'] = "Validator is not opened!";			
 			return false;
 		}
@@ -62,7 +87,7 @@ class CashCode{
 					if ($this->BillToBill_ExtendedCode[$Code][$ExtendedCode] != "") {
 						$massage .= " - ".$this->BillToBill_ExtendedCode[$Code][$ExtendedCode];
 					}
-					$this->info(['massage' => $massage])
+					$this->info(['massage' => $massage]);
 					switch ($Code)
 					{
 						case 0x43:
--- app/Http/Controllers/cashController.php
+++ app/Http/Controllers/cashController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Cash;
 use Server1C;
+use CashCode;
 class cashController extends Controller
 {
     public function summ()
