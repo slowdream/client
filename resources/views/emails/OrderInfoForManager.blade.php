b62e2a4fade0ac463142ea93a798396bb42f69a6
slowdream <slowdream@yandex.ru>
2017-11-10 13:52:54 +0300

    Отправка имейла и сообщения при оформлении заказа.

---
+++ resources/views/emails/OrderInfoForManager.blade.php
@@ -0,0 +1,13 @@
+На терминале {{ $idterm }} создан заказ номер {{ $orderId }} на {{ count($products) }} позиций.
+Контакты заказчика {{ $name }} {{ $telnumber }}
+Адрес доставки {{ $address }}
+Дата и время доставки {{ $orderDate }} {{ $timeRange }}
+Общая сумма заказа {{ $summ }}р + доставка {{ $delivery }}р
+Было оплачено на месте {{ $pay }}р
+@if($pay > $summ)
+Выдать сдачу в размере {{ $pay - $summ }}р
+@endif
+Позиции:
+@foreach($products as $product)
+{{ $product['guid'] }} {{ $product['name'] }} {{ $product['count'] }}{{ $product['unit'] }} {{ $product['price'] }}р
+@endforeach
