14f8bb617b27c96418ef684c49e99dc68973b0f6
slowdream <slowdream@yandex.ru>
2018-01-15 16:08:37 +0300

    Добавлена тестовая команда для чеков

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Foundation\Inspiring;
+use Carbon\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,4 +23,34 @@ Artisan::command('GetProducts', function () {
 });
 Artisan::command("SendOrdersToServer {id=''}", function ($id) {
     dispatch(new App\Jobs\SendOrdersToServer($id));
+});
+
+Artisan::command('testprinter', function () {
+    $data = [
+      'products' => [
+        [
+          'guid' => '66666',
+          'name' => 'ttteeest',
+          'count' => '3',
+          'price' => '666',
+        ]
+      ],
+      'summ' => 666,
+      'cash' => 6666,
+      'tel' => '666666',
+      'address' => 'gdfgdfgdfgdfgfddf',
+      'id' => '6666',
+      'date' => Carbon::now('Europe/Moscow')->toDateTimeString(),
+      'orderDate' => '66.66.66',
+      'timeRange' => '121-122',
+      'reason' => 'test',
+      'delivery' => 300
+    ];
+    //Четвертое число в размере бумаги это высота чека и его нужно вычислять заранее
+    $pdf = PDF::loadView('check/receipt', $data)
+      ->setPaper([0, 0, 218, 500], 'portrait');
+    //return $pdf->stream();
+    $pdf->save(resource_path('reciepts/reciept.pdf'));
+    $file = resource_path('reciepts/reciept.pdf');
+    $print = `lp {$file}`;
 });
